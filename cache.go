460d0bc108662ac10c51cd700057167d1169a4a2
Tristan Rice <rice@fn.lc>
2016-02-14 02:41:07 -0800

    Lowered cache amount

--- cache.go
+++ cache.go
@@ -9,12 +9,12 @@ func (s *Server) initCache() {
 	s.zoomedFloorCache = groupcache.GetGroup("zoomedImages")
 	if s.zoomedFloorCache == nil {
 		g := zoomedImageGetter{s}
-		s.zoomedFloorCache = groupcache.NewGroup("zoomedImages", int64(500*units.MiB), g)
+		s.zoomedFloorCache = groupcache.NewGroup("zoomedImages", int64(256*units.MiB), g)
 	}
 
 	s.tileCache = groupcache.GetGroup("mapTiles")
 	if s.tileCache == nil {
 		g := mapTileGetter{s}
-		s.tileCache = groupcache.NewGroup("mapTiles", int64(500*units.MiB), g)
+		s.tileCache = groupcache.NewGroup("mapTiles", int64(256*units.MiB), g)
 	}
 }
